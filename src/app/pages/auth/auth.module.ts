0217bb41691280b9e83caed15b5b23a92832e715
thinhnt@vi-mash.com <thinhnt@vi-mash.com>
2022-07-05 09:10:52 +0700

    ThinhNT: update 20220705

---
+++ src/app/pages/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '../angular-material.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    AngularMaterialModule
+  ]
+})
+export class AuthModule { }
